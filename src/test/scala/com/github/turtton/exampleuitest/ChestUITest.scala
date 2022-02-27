d893535d8e6bb43ab8282e1bf69199dc606e8e41
turtton <watagame2579@gmail.com>
2022-02-27 14:58:07 +0900

    :recycle: reformat

--- src/test/scala/com/github/turtton/exampleuitest/ChestUITest.scala
+++ src/test/scala/com/github/turtton/exampleuitest/ChestUITest.scala
@@ -7,36 +7,25 @@ import io.github.kory33.s2mctest.core.client.api.worldview.{PositionAndOrientati
 import io.github.kory33.s2mctest.core.client.api.{DiscretePath, MinecraftVector, Vector2D}
 import io.github.kory33.s2mctest.core.client.{PacketAbstraction, ProtocolPacketAbstraction}
 import io.github.kory33.s2mctest.core.clientpool.{AccountPool, ClientPool}
-import io.github.kory33.s2mctest.impl.client.abstraction.{
-  DisconnectAbstraction,
-  KeepAliveAbstraction,
-  PlayerPositionAbstraction,
-  TimeUpdateAbstraction
-}
+import io.github.kory33.s2mctest.impl.client.abstraction.{DisconnectAbstraction, KeepAliveAbstraction, PlayerPositionAbstraction, TimeUpdateAbstraction}
 import io.github.kory33.s2mctest.impl.client.api.MoveClient
 import io.github.kory33.s2mctest.impl.clientpool.ClientInitializationImpl
-import io.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Play.ServerBound.{
-  Player,
-  PlayerLook,
-  PlayerPosition,
-  PlayerPositionLook
-}
+import io.github.kory33.s2mctest.impl.connection.packets.PacketIntent.Play.ServerBound.{Player, PlayerLook, PlayerPosition, PlayerPositionLook}
 import monocle.Lens
 import monocle.macros.GenLens
-
-import scala.concurrent.duration.FiniteDuration
-
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest
 import net.minecraft.test.{GameTest, TestContext}
 
+import scala.concurrent.duration.FiniteDuration
+
 
 class ChestUITest extends FabricGameTest {
-  import io.github.kory33.s2mctest.impl.connection.protocol.versions
-  import versions.v1_17_1.{protocolVersion, loginProtocol, playProtocol}
   import cats.effect.unsafe.implicits.global
-
   import cats.implicits.given
+  import io.github.kory33.s2mctest.impl.connection.protocol.versions
   import spire.implicits.given
+  import versions.v1_17_1.{loginProtocol, playProtocol, protocolVersion}
+
   import scala.concurrent.duration.given
 
   @GameTest(structureName = FabricGameTest.EMPTY_STRUCTURE)
@@ -74,15 +63,11 @@ class ChestUITest extends FabricGameTest {
       }
       .void
 
-
-
-    //TODO: connect TestClient and wait
     context.waitAndRun(60, () => {
       val player = context.getWorld.getPlayers.get(0)
       ChestUI.open(player)
       context.complete()
     })
-
 
     program.unsafeRunAsync(_ => {})
 
