ce84a6cf11319a0146c835ea88b564737f8612b1
turtton <watagame2579@gmail.com>
2022-03-01 19:32:13 +0900

    :bulb: add comments

--- src/test/scala/com/github/turtton/exampleuitest/ChestUITest.scala
+++ src/test/scala/com/github/turtton/exampleuitest/ChestUITest.scala
@@ -64,12 +64,14 @@ class ChestUITest extends FabricGameTest {
               case _: JoinGame_WorldNames_IsHard => IO.pure(None)
               case _: DeclareRecipes => IO {
                 context.getWorld.getServer.execute(() => {
+                    //Open Chest Inventory Window
                     val player = context.getWorld.getServer.getPlayerManager.getPlayer(client.identity.uuid)
                     println("Open Window")
                     ChestUI.open(player)
                 })} >> IO.pure(None)
               case windowPacket: WindowOpen => IO {
                 println(s"Window opened!${windowPacket.title.json}")
+                //TODO: click slot 0 item and check item name
                 context.getWorld.getServer.execute(() => {
                   context.complete()
                 })
