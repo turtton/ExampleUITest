a6f25ab1c16d7b4f0af92272bb15411e1a76cdc9
turtton <watagame2579@gmail.com>
2022-03-05 15:50:20 +0900

    :hammer: make texts to singleton objects

--- src/main/scala/com/github/turtton/exampleuitest/ChestUI.scala
+++ src/main/scala/com/github/turtton/exampleuitest/ChestUI.scala
@@ -1,14 +1,21 @@
 package com.github.turtton.exampleuitest
 
+import com.github.turtton.exampleuitest.ChestUI.{ITEM_DISPLAY_CLICKED, ITEM_DISPLAY_CLICK_ME}
 import net.minecraft.entity.player.{PlayerEntity, PlayerInventory}
 import net.minecraft.inventory.{Inventory, SimpleInventory}
 import net.minecraft.item.{Item, ItemStack, Items}
 import net.minecraft.screen.slot.{Slot, SlotActionType}
-import net.minecraft.screen.{ScreenHandler, ScreenHandlerFactory, ScreenHandlerType, SimpleNamedScreenHandlerFactory}
+import net.minecraft.screen.{
+  ScreenHandler,
+  ScreenHandlerFactory,
+  ScreenHandlerType,
+  SimpleNamedScreenHandlerFactory
+}
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.LiteralText
 
-class ChestUI private (syncId: Int, val player: PlayerEntity) extends ScreenHandler(ScreenHandlerType.GENERIC_9X1, syncId) {
+class ChestUI private (syncId: Int, val player: PlayerEntity)
+    extends ScreenHandler(ScreenHandlerType.GENERIC_9X1, syncId) {
 
   val size = 9
   val inventory = new SimpleInventory(1)
@@ -29,10 +36,14 @@ class ChestUI private (syncId: Int, val player: PlayerEntity) extends ScreenHand
     addSlot(new ImmutableSlot(playerInventory, x + y * 9 + 9))
   }
 
-
   override def canUse(player: PlayerEntity): Boolean = true
 
-  override def onSlotClick(i: Int, j: Int, actionType: SlotActionType, playerEntity: PlayerEntity): Unit = {
+  override def onSlotClick(
+    i: Int,
+    j: Int,
+    actionType: SlotActionType,
+    playerEntity: PlayerEntity
+  ): Unit = {
     if (i == 0 && j == 0) {
       isClicked = !isClicked
       setStackInSlot(0, 0, getItem)
@@ -42,7 +53,7 @@ class ChestUI private (syncId: Int, val player: PlayerEntity) extends ScreenHand
 
   private def getItem: ItemStack = {
     val item = Items.STICK.getDefaultStack.copy()
-    val name = if (isClicked) LiteralText("Clicked") else LiteralText("ClickMe")
+    val name = if (isClicked) ITEM_DISPLAY_CLICKED else ITEM_DISPLAY_CLICK_ME
     item.setCustomName(name)
     item
   }
@@ -51,10 +62,19 @@ class ChestUI private (syncId: Int, val player: PlayerEntity) extends ScreenHand
     override def canInsert(stack: ItemStack): Boolean = false
     override def canTakeItems(playerEntity: PlayerEntity): Boolean = false
   }
-
-
 }
 
-object ChestUI{
-  def open(player: ServerPlayerEntity): Unit = player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId: Int, _: PlayerInventory, player: PlayerEntity) => new ChestUI(syncId, player), new LiteralText("Test")))
+object ChestUI {
+
+  val TITLE: LiteralText = LiteralText("Test")
+
+  val ITEM_DISPLAY_CLICKED: LiteralText = LiteralText("Clicked")
+  val ITEM_DISPLAY_CLICK_ME: LiteralText = LiteralText("ClickMe")
+
+  def open(player: ServerPlayerEntity): Unit = player.openHandledScreen(
+    new SimpleNamedScreenHandlerFactory(
+      (syncId: Int, _: PlayerInventory, player: PlayerEntity) => new ChestUI(syncId, player),
+      TITLE
+    )
+  )
 }
